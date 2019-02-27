2602c7a8782c7db51a19b156efeb0074cebe0ee6
13509696075 <13509696075@139.com>
2019-02-27 22:33:53 +0800

    添加项目文件。

---
+++ DjangoWebProject1/env/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!C:\Users\dell\Desktop\Visual Studio 2017\学习\python网页程式交易APP实作\DjangoWebProject1\DjangoWebProject1\env\Scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
